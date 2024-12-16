4c8ef679d8ba13f5cdcf2b2fc27a925a3a4f89b5
Sunwoo Kim (Mac) <sunshower1127@gmail.com>
2024-12-16 13:53:03 +0900

    업데이트: 이제 게임, 미디어도 바텀업 돌릴 수 있음

--- picker.js
+++ picker.js
@@ -4,7 +4,22 @@ const { exec } = require("child_process");
 require("dotenv").config();
 
 const key = process.env.NOTION_KEY;
-const db = process.env.NOTION_DB;
+
+const DbIds = {
+  study: process.env.STUDY_DB,
+  game: process.env.GAME_DB,
+  media: process.env.MEDIA_DB,
+};
+const doneListPaths = {
+  study: __dirname + "/pickedDataStudy.json",
+  game: __dirname + "/pickedDataGame.json",
+  media: __dirname + "/pickedDataMedia.json",
+};
+
+const mainInterface = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 // Initializing a client
 const notion = new Client({
@@ -15,24 +30,41 @@ function sortMap(map) {
   return new Map([...map.entries()].sort((a, b) => b[1] - a[1]));
 }
 
-async function loadList() {
-  const response = await notion.databases.query({
-    database_id: db,
+function mergeMaps(...maps) {
+  const mergedMap = new Map();
+
+  maps.forEach((map) => {
+    map.forEach((value, key) => {
+      mergedMap.set(key, (mergedMap.get(key) || 0) + value);
+    });
   });
 
+  return mergedMap;
+}
+
+async function fetchTodoList(dbName) {
+  const dbId = DbIds[dbName];
   const data = new Map();
 
+  const response = await notion.databases.query({
+    database_id: dbId,
+  });
   response.results.forEach(
     ({
       properties: {
         비중: { number },
-        이름: {
+        진행상황: {
           title: [{ plain_text: title }],
         },
       },
     }) => {
       if (number === 0) return;
-      data.set(title, number);
+      const newTitle = title.split(" |")[0];
+      if (data.has(newTitle)) {
+        data.set(newTitle, data.get(newTitle) + number);
+      } else {
+        data.set(newTitle, number);
+      }
     }
   );
 
@@ -40,12 +72,22 @@ async function loadList() {
 }
 
 // 로컬의 pickedData.json에서 Map 데이터를 불러와서 반환
-async function loadPickedData(path) {
-  if (!fs.existsSync(path)) return new Map();
-  const data = fs.readFileSync(path);
-
-  if (data.length === 0) return new Map();
-  return new Map(Object.entries(JSON.parse(data)));
+async function readDoneFile(path) {
+  try {
+    const data = await fs.promises.readFile(path);
+    const mapData = new Map(Object.entries(JSON.parse(data)));
+    const combinedData = new Map();
+    mapData.forEach((value, key) => {
+      combinedData.set(key, (combinedData.get(key) || 0) + value);
+    });
+    return combinedData;
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      await fs.promises.writeFile(path, JSON.stringify({}));
+      return new Map();
+    }
+    throw error;
+  }
 }
 
 function mapToObject(map) {
@@ -56,11 +98,8 @@ function mapToObject(map) {
   return obj;
 }
 
-function writePickedData(data) {
-  fs.writeFileSync(
-    __dirname + "/pickedData.json",
-    JSON.stringify(mapToObject(data), null, 2)
-  );
+function writeDoneData(path, data) {
+  fs.writeFileSync(path, JSON.stringify(mapToObject(data), null, 2));
 }
 
 function pickRandom(data) {
@@ -93,49 +132,81 @@ function subtractMaps(totalData, pickedData) {
 }
 
 async function main() {
-  const totalData = await loadList();
+  mainInterface.question(
+    "(s)tudy/(m)edia/(g)ame 선택(중복가능): ",
+    async (choice) => {
+      let selectedDbs = [];
 
-  const pickedData = await loadPickedData(__dirname + "/pickedData.json");
-
-  let remainingData = subtractMaps(totalData, pickedData);
-
-  if (remainingData.size === 0) {
-    console.log("바텀업을 모두 완료했습니다. pickedData를 비워주세요.");
-    return;
-  }
-
-  const randomOne = pickRandom(remainingData);
-
-  console.log(randomOne);
-  exec(`printf "${randomOne}" | pbcopy`);
-
-  // pickedData.set(randomOne, (pickedData.get(randomOne) || 0) + 1);
-  // writePickedData(pickedData);
-
-  const stopwatch = require("./stopwatch");
-
-  stopwatch.rl.input.once("keypress", (char, key) => {
-    stopwatch.stopStopwatch();
-    stopwatch.rl.question("몇시간 짜리인지: ", (input) => {
-      const number = parseInt(input, 10);
-      if (number) {
-        pickedData.set(
-          randomOne,
-          (pickedData.get(randomOne) || 0) + number /* -1 */
-        );
-        writePickedData(sortMap(pickedData));
-        console.log(`${randomOne}: ${pickedData.get(randomOne)}`);
+      if (choice.includes("s")) selectedDbs.push("study");
+      if (choice.includes("m")) selectedDbs.push("media");
+      if (choice.includes("g")) selectedDbs.push("game");
+      if (selectedDbs.length === 0) {
+        console.log("잘못된 선택입니다.");
+        mainInterface.close();
+        return;
       }
-      remainingData = subtractMaps(totalData, pickedData);
-      // console.log(remainingData);
-      const sum = [...remainingData.values()].reduce(
-        (acc, cur) => acc + cur,
-        0
+
+      let subTodoData = {};
+
+      await Promise.all(
+        selectedDbs.map(async (db) => {
+          subTodoData[db] = await fetchTodoList(db);
+        })
       );
-      console.log(`남은 바텀업: ${sum}개`);
-      stopwatch.rl.close();
-    });
-  });
+
+      const todoData = mergeMaps(...Object.values(subTodoData));
+
+      let subDoneData = {};
+
+      await Promise.all(
+        selectedDbs.map(async (db) => {
+          subDoneData[db] = await readDoneFile(doneListPaths[db]);
+        })
+      );
+
+      const doneData = mergeMaps(...Object.values(subDoneData));
+
+      let remainingData = subtractMaps(todoData, doneData);
+
+      if (remainingData.size === 0) {
+        console.log("바텀업을 모두 완료했습니다. pickedData를 비워주세요.");
+        return;
+      }
+
+      const randomOne = pickRandom(remainingData);
+
+      console.log(randomOne);
+      exec(`printf "${randomOne}" | pbcopy`);
+
+      // pickedData.set(randomOne, (pickedData.get(randomOne) || 0) + 1);
+      // writePickedData(pickedData);
+
+      mainInterface.close();
+      const stopwatch = require("./stopwatch");
+
+      stopwatch.rl.input.once("keypress", (char, key) => {
+        stopwatch.stopStopwatch();
+        stopwatch.rl.question("몇시간 짜리인지: ", (input) => {
+          const number = parseInt(input, 10);
+          if (number) {
+            const db = Object.entries(subTodoData).find(([key, value]) =>
+              value.has(randomOne)
+            )[0];
+
+            subDoneData[db].set(
+              randomOne,
+              (subDoneData[db].get(randomOne) || 0) + number
+            );
+
+            writeDoneData(doneListPaths[db], sortMap(subDoneData[db]));
+
+            console.log(`${randomOne}: ${subDoneData[db].get(randomOne)}`);
+          }
+          stopwatch.rl.close();
+        });
+      });
+    }
+  );
 }
 
 main();
